883ece2176c8f49364cbb2ec13e9fea0e8614935
samandar <samandar@samandarui-MacBookAir.local>
2022-11-14 22:37:50 +0900

    removed card regex

--- handlers/users/register.py
+++ handlers/users/register.py
@@ -4,7 +4,7 @@ from loader import dp
 
 
 PHONE_NUM = r'^[0-9]{3}-([0-9]{3}|[0-9]{4})-[0-9]{4}$'
-KOREAN_CARD = r'^9[0-9]{15}$'
+#KOREAN_CARD = r'^9[0-9]{15}$'
 
 
 
@@ -26,14 +26,14 @@ async def regexp_phone(msg: types.Message, state: FSMContext):
    
     ##  moving to another state 
     await state.set_state('card_number')
-    text = (f"{msg.from_user.full_name}!",
+    text = (f"{msg.from_user.full_name}",
             "endi iltimos, Koreya plastik karta raqamingizni kiriting!",
             "Misol uchun: 010-9552-4141")
     await msg.answer("\n".join(text))
 
 
 
-@dp.message_handler(filters.Regexp(KOREAN_CARD),state='card_number')
+@dp.message_handler(state='card_number')
 async def state_example(msg: types.Message, state: FSMContext):
     await msg.answer("Karta raqamingiz muvaffaqiyatli  saqlandi!")
     await msg.answer(f"{msg.from_user.full_name}, royhatdan muvaffaqiyatli o'tkaniz bilan tabriklaymiz!")
