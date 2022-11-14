f29b39f32952b495646a70942c0eb47e86295f56
samandar <samandar@samandarui-MacBookAir.local>
2022-11-14 15:31:10 +0900

    new template for bot

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
+from aiogram import executor
 
-import logging
-from aiogram import Bot, Dispatcher, executor, types
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-API_TOKEN = os.getenv("API_TOKEN")
+from loader import dp
+import middlewares, filters, handlers
+from utils.notify_admins import on_startup_notify
+from utils.set_bot_commands import set_default_commands
 
 
+async def on_startup(dispatcher):
+    # Birlamchi komandalar (/star va /help)
+    await set_default_commands(dispatcher)
 
-#Configure logging
-logging.basicConfig(level=logging.INFO)
-
-# Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-
-
-
-
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Assalomu Aleykum, Ish Bor botimizga xush kelibsiz!")
-
-
-@dp.message_handler(commands=['help'])
-async def send_welcome(message: types.Message):
-    await message.reply("Bot buyicha savollarizngiz bo'lsa @khodjiyev2o ga murojaat qilishingiz mumkin!")
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
-
-
-
-
+    # Bot ishga tushgani haqida adminga xabar berish
+    await on_startup_notify(dispatcher)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, on_startup=on_startup)
