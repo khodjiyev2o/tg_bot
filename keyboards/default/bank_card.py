db57aeb648aa42a8f6a1726984286efa1c4814e8
samandar <samandar@samandarui-MacBookAir.local>
2022-11-15 20:40:47 +0900

    bank_name buttons added

---
+++ keyboards/default/bank_card.py
@@ -0,0 +1,43 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram import types
+
+
+bank_card = ReplyKeyboardMarkup(resize_keyboard=True)
+
+cards = [
+    'KEB Hana Bank',
+    'KB Kookmin Bank',
+    'Standard Chartered',
+    'Standard Chartered Korea',
+    'Shinhan Bank',
+    'Woori Bank',
+    'Nonghyup Bank(농협)',
+    'SC che il',
+    'Citibank Korea',
+    'HSBC',
+    'KAKAO BANK',
+    'doiji bank',
+    'BOA',
+    'JP MOGAN',
+    'Chungug kungsan',
+    'BNP',
+    'Kei bank',
+    'MG BANK(SEMAILKIMKO)',
+    'CHUNGUG',
+    'TOSIBANK',
+    'KOKSE',
+    'DGB Financial Group(Daegu Bank)',
+    'BNK Financial Group',
+    'Busan Bank',
+    'Kyongnam Bank',
+    'JB Financial Group',
+    'Kwangju Bank',
+    'Jeonbuk Bank',
+    'Shinhan Financial Group',
+    'Jeju Bank'
+]
+
+for x in cards:
+  bank_card.add(KeyboardButton(x))
+
+
